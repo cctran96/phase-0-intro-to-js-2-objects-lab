f984a140a754b802f9c1a406c04833bd927e03b4
Chau Tran <cctran96@gmail.com>
2021-04-12 20:51:48 -0700

    commit tree a3f6b4f2b6ef68726cc84c039b8d823c9bf75fc4

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var employee = {name: 'Chau', streetAdress: '123 ABC St'}
+const employee = {}
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    var newObject = Object.assign({}, employee)
+    var newObject = {...employee}
     newObject[key] = value
     return newObject
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value
-    return employee
+   employee[key] = value
+   return employee
 }
 
 function deleteFromEmployeeByKey(employee, key) {
-    var newObject = Object.assign({}, employee)
+    var newObject = {...employee}
     delete newObject[key]
     return newObject
 }
